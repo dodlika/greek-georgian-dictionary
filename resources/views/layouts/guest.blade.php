81097ef7a9ae7ee923f7337393c7b58c6e772259
Dod M <Dodlika123@gmail.com>
2025-07-22 03:02:39 +0300

    123

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,7 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <!-- Scripts -->
     </head>
     <body class="font-sans text-gray-900 antialiased">
@@ -25,5 +25,6 @@
                 {{ $slot }}
             </div>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
